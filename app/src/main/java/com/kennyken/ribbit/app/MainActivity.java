139b7f273c76ba0f5401e8a437679c79118c42a4
Kendrick Ledet <kendrickledet@gmail.com>
2014-05-31 09:25:10 -0500

    Completed code for building and sending messages with file attachments via Parse.

--- app/src/main/java/com/kennyken/ribbit/app/MainActivity.java
+++ app/src/main/java/com/kennyken/ribbit/app/MainActivity.java
@@ -267,6 +267,15 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
             // Send user to recipients selection to send set media uri
             Intent recipientsIntent = new Intent(this, RecipientsActivity.class);
             recipientsIntent.setData(mMediaUri);
+
+            String fileType;
+            if (requestCode == PICK_PHOTO_REQUEST || requestCode == TAKE_PHOTO_REQUEST) {
+                fileType = ParseConstants.TYPE_IMAGE;
+            } else {
+                fileType = ParseConstants.TYPE_VIDEO;
+            }
+            recipientsIntent.putExtra(ParseConstants.KEY_FILE_TYPE, fileType);
+
             startActivity(recipientsIntent);
 
         } else if (resultCode != RESULT_CANCELED) {
--- app/src/main/java/com/kennyken/ribbit/app/RecipientsActivity.java
+++ app/src/main/java/com/kennyken/ribbit/app/RecipientsActivity.java
@@ -1,6 +1,7 @@
 package com.kennyken.ribbit.app;
 
 import android.app.ListActivity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
@@ -13,9 +14,13 @@ import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
 import com.parse.ParseRelation;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -27,6 +32,8 @@ public class RecipientsActivity extends ListActivity {
     protected ParseUser mCurrentUser;
 
     protected MenuItem mSendMenuItem;
+    protected Uri mMediaUri;
+    protected String mFileType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +43,14 @@ public class RecipientsActivity extends ListActivity {
         setContentView(R.layout.activity_recipients);
 
         getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+        mMediaUri = getIntent().getData();
+        mFileType = getIntent().getExtras().getString(ParseConstants.KEY_FILE_TYPE);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
 
         mCurrentUser = ParseUser.getCurrentUser();
         mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
@@ -78,7 +93,6 @@ public class RecipientsActivity extends ListActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.recipients, menu);
         mSendMenuItem = menu.getItem(0);
         mSendMenuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
@@ -88,15 +102,20 @@ public class RecipientsActivity extends ListActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         switch (id) {
             case android.R.id.home:
                 NavUtils.navigateUpFromSameTask(this);
                 return true;
             case R.id.action_send:
+                ParseObject message = createMessage();
+                if ( message == null ) {
+                    // error building message
+                    Toast.makeText(this, R.string.file_build_error, Toast.LENGTH_LONG).show();
+                } else {
+                    send(message);
+                    finish();  // exit this activity, return to MainActivity
+                }
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -110,5 +129,59 @@ public class RecipientsActivity extends ListActivity {
             mSendMenuItem.setVisible(true);
         } else
             mSendMenuItem.setVisible(false);
+    }
+
+    /*
+     * Build message to be sent as ParseObject
+     */
+    protected ParseObject createMessage() {
+        ParseObject message = new ParseObject(ParseConstants.CLASS_MESSAGES);
+        message.put(ParseConstants.KEY_SENDER_ID, ParseUser.getCurrentUser().getObjectId());
+        message.put(ParseConstants.KEY_SENDER_NAME, ParseUser.getCurrentUser().getUsername());
+        message.put(ParseConstants.KEY_RECIPIENT_IDS, getRecipientIds());
+        message.put(ParseConstants.KEY_FILE_TYPE, mFileType);
+
+        // Process and attach media file
+        byte[] fileBytes = FileHelper.getByteArrayFromFile(this, mMediaUri);
+
+        if ( fileBytes == null )
+            return null;
+
+        if ( mFileType.equals(ParseConstants.TYPE_IMAGE) )
+            fileBytes = FileHelper.reduceImageForUpload(fileBytes);
+
+        String fileName = FileHelper.getFileName(this, mMediaUri, mFileType);
+        ParseFile file = new ParseFile(fileName, fileBytes);
+        message.put(ParseConstants.KEY_FILE, file);
+
+        return message;
+    }
+
+    protected void send(ParseObject message) {
+        message.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if ( e == null ) {
+                    Toast.makeText(RecipientsActivity.this, R.string.message_sent,
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(RecipientsActivity.this, R.string.message_not_sent,
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    /*
+     * Get object ids for each checked recipient
+     */
+    protected ArrayList<String> getRecipientIds() {
+        ArrayList<String> recipientIds = new ArrayList<String>();
+        for ( int i=0; i < getListView().getCount(); i++ ) {
+            if ( getListView().isItemChecked(i) )
+                recipientIds.add(mFriends.get(i).getObjectId());
+        }
+
+        return recipientIds;
     }
 }
