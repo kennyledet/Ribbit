30164165746de251afa1b415cd9d2718aafb49fb
Kendrick Ledet <kendrickledet@gmail.com>
2014-06-01 08:25:36 -0500

    Added custom layout and ArrayAdapter (MessageAdapter) for message list items in the Inbox

--- app/src/main/java/com/kennyken/ribbit/app/InboxFragment.java
+++ app/src/main/java/com/kennyken/ribbit/app/InboxFragment.java
@@ -5,7 +5,6 @@ import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -54,17 +53,10 @@ public class InboxFragment extends ListFragment {
 
                 if (e == null) {
                     mMessages = messages;
-                    String[] usernames = new String[mMessages.size()];
-                    int i = 0;
-                    for (ParseObject message : mMessages) {
-                        usernames[i] = message.getString(ParseConstants.KEY_SENDER_NAME);
-                        i++;
-                    }
 
-                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                    MessageAdapter adapter = new MessageAdapter(
                             getListView().getContext(),
-                            android.R.layout.simple_list_item_1,
-                            usernames
+                            mMessages
                     );
                     setListAdapter(adapter);
                 }
---
+++ app/src/main/java/com/kennyken/ribbit/app/MessageAdapter.java
@@ -0,0 +1,80 @@
+package com.kennyken.ribbit.app;
+
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.parse.ParseObject;
+
+import java.util.List;
+
+/*
+ * Custom ArrayAdapter for Inbox message list items (as ParseObjects)
+ */
+public class MessageAdapter extends ArrayAdapter<ParseObject>{
+    protected Context mContext;
+    protected List<ParseObject> mMessages;
+
+    public MessageAdapter(Context context, List<ParseObject> messages) {
+        super(context, R.layout.message_item, messages);
+
+        mContext = context;
+        mMessages = messages;
+    }
+
+    /*
+     * Convert vanilla view to message-specific layout
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.message_item, null);
+
+            holder = new ViewHolder();
+            holder.iconImageView = (ImageView) convertView.findViewById(R.id.messageIcon);
+            holder.nameLabel = (TextView) convertView.findViewById(R.id.senderLabel);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        ParseObject message = mMessages.get(position);
+
+        String fileType = message.getString(ParseConstants.KEY_FILE_TYPE);
+        int imageResource = getIconImageResource(fileType);
+        Log.i("test", "Rsrc id: " + imageResource);
+        holder.iconImageView.setImageResource(R.drawable.ic_action_picture);
+        holder.nameLabel.setText(message.getString(ParseConstants.KEY_SENDER_NAME));
+
+        return convertView;
+    }
+
+    /*
+     * Includes data to be displayed in custom layout
+     */
+    private static class ViewHolder {
+        ImageView iconImageView;
+        TextView nameLabel;
+    }
+
+    private int getIconImageResource(String key) {
+        int imageResource = -1;
+        if ( key.equals(ParseConstants.TYPE_IMAGE) ) {
+            imageResource = R.drawable.ic_action_picture;
+        } else if ( key.equals(ParseConstants.TYPE_VIDEO) ) {
+            imageResource = R.drawable.ic_action_play_over_video;
+        } else if ( key.equals(ParseConstants.TYPE_TEXT_MESSAGE) ) {
+            imageResource = R.drawable.ic_action_play_over_video; // temp
+        }
+        return imageResource;
+    }
+}
